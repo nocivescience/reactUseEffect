6b83fd1712c069c80695f02666c449e861595c91
noceive <nocivescience@gmail.com>
2023-05-10 20:04:18 -0400

    funciona con algo más complejo en useEffect

--- ejemplo1/src/App.jsx
+++ ejemplo1/src/App.jsx
@@ -5,9 +5,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [eventData, setEventData] = useState(null)
   useEffect(() => {
-    console.log(`count is: ${count}`)
-  }, [count])
+    const eventListener = (event) => {
+      setEventData(event)
+      console.log(event)
+    }
+    window.addEventListener('click', eventListener)
+    return () => {
+      window.removeEventListener('click', eventListener);
+      console.log('Event listener removed')
+    }
+  }, [])
+  const handleButtonClick = () => {
+    setCount(count=>count+1)
+    const eventData = {message: 'Event emitted', timesptamp: new Date().getTime()};
+    const customEvent = new CustomEvent('custom-event', {detail: eventData});
+    window.dispatchEvent(customEvent);
+  }
   return (
     <>
       <div>
@@ -20,13 +35,23 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={handleButtonClick}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
+      {
+        eventData && (
+          <div className="event-data-container">
+          <h2>Event Data</h2>
+          <pre>
+            {JSON.stringify(eventData, null, 2)}
+          </pre>
+        </div>
+        )
+      }
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
